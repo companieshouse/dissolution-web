60c52f28abd042e168a1542962fa4f23f7ce4811
mateuszc-kainos <mateuszc@kainos.com>
2020-06-05 18:12:02 +0200

    Add middleware to SearchCompanyController

--- src/controllers/searchCompany.controller.ts
+++ src/controllers/searchCompany.controller.ts
@@ -1,9 +1,11 @@
+import { SessionMiddleware } from 'ch-node-session-handler'
 import { controller, httpGet } from 'inversify-express-utils'
 
 import BaseController from 'app/controllers/base.controller'
+import { AuthMiddleware } from 'app/middleware/auth.middleware'
 import { SEARCH_COMPANY_URI } from 'app/paths'
 
-@controller(SEARCH_COMPANY_URI)
+@controller(SEARCH_COMPANY_URI, SessionMiddleware, AuthMiddleware)
 export class SearchCompanyController extends BaseController {
 
   @httpGet('')
