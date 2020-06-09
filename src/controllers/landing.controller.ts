9af9ee7025636151b14c44671ec92ce13d32478c
Sapphire Duffy <sduffy@pd15884.home>
2020-06-09 14:48:40 +0100

    Format changes

--- src/controllers/landing.controller.ts
+++ src/controllers/landing.controller.ts
@@ -1,7 +1,7 @@
 import { controller, httpGet, httpPost } from 'inversify-express-utils'
 
 import BaseController from 'app/controllers/base.controller'
-import { WHO_TO_TELL_URI, ROOT_URI } from 'app/paths'
+import { ROOT_URI, WHO_TO_TELL_URI } from 'app/paths'
 
 @controller(ROOT_URI)
 export class LandingController extends BaseController {
--- test/controllers/landing.controller.test.ts
+++ test/controllers/landing.controller.test.ts
@@ -11,20 +11,20 @@ import { ROOT_URI, WHO_TO_TELL_URI } from "app/paths";
 
 const app: Application = createApp();
 
-describe("LandingController", () => {
-  describe("GET request", () => {
-    it("should render the landing page", async () => {
+describe('LandingController', () => {
+  describe('GET request', () => {
+    it('should render the landing page', async () => {
       const res = await request(app).get(ROOT_URI).expect(OK);
-      assert.include(res.text,"Use this service to apply to close a company, also known as striking a company off the register or dissolution.")
+      assert.include(res.text,'Use this service to apply to close a company, also known as striking a company off the register or dissolution.')
     })
   })
 
-  describe("POST request", () => {
-    it("should redirect to the who to tell screen upson submission", async () => {
+  describe('POST request', () => {
+    it('should redirect to the who to tell screen upson submission', async () => {
       await request(app)
         .post(ROOT_URI)
         .expect(MOVED_TEMPORARILY)
-        .expect("Location", WHO_TO_TELL_URI);
+        .expect('Location', WHO_TO_TELL_URI);
     })
   })
 })
