ce9451d4979a965e958b1eb8e2c8b4cb94055937
Sapphire Duffy <sduffy@pd15884.home>
2020-06-09 13:49:36 +0100

    Added unit tests

--- test/controllers/landing.controller.test.ts
+++ test/controllers/landing.controller.test.ts
@@ -2,19 +2,26 @@ import 'reflect-metadata'
 
 import { assert } from 'chai'
 import { Application } from 'express'
-import { OK } from 'http-status-codes'
+import { OK, MOVED_TEMPORARILY } from 'http-status-codes'
 import request from 'supertest'
 
 import { createApp } from 'app/application.factory'
 import 'app/controllers/landing.controller'
-import { ROOT_URI } from 'app/paths'
+import { ROOT_URI, WHO_TO_TELL_URI } from 'app/paths'
 
 const app: Application = createApp()
 
 describe('LandingController', () => {
   it('should render the landing page', async () => {
     const res = await request(app).get(ROOT_URI).expect(OK)
-
-    assert.include(res.text, 'Use this service to get your NodeJS application up and running')
+    assert.include(res.text, 'Use this service to apply to close a company, also known as striking a company off the register or dissolution.')
+    })
   })
-})
+
+  describe('POST request', () => {
+    it('should redirect to the who to tell screen upson submission', async () => {
+    await request(app).post(ROOT_URI).expect(MOVED_TEMPORARILY).expect('Location', WHO_TO_TELL_URI)
+    })
+  })
+
+
