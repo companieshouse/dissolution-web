2b21f5c4c7d713999b02ebe16534c479f8df9520
sapphireduffy <sapphireduffy@gmail.com>
2020-07-30 15:33:34 +0100

    Controller, Static page and unit test

---
+++ src/controllers/certificateSigned.controller.ts
@@ -0,0 +1,16 @@
+
+import { controller, httpGet } from 'inversify-express-utils'
+
+import BaseController from 'app/controllers/base.controller'
+import { CERTIFICATE_SIGNED_URI } from 'app/paths'
+import TYPES from 'app/types'
+
+@controller(CERTIFICATE_SIGNED_URI, TYPES.SessionMiddleware, TYPES.AuthMiddleware, TYPES.CompanyAuthMiddleware)
+export class CertificateSignedController extends BaseController {
+
+  @httpGet('')
+  public async get(): Promise<string> {
+
+    return super.render('certificate-signed')
+  }
+}
