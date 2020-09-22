6083f3c1aa81de43c7ba9db295c9b462faa77a38
Mateusz Czarnowski <mateuszc@kainos.com>
2020-09-22 15:13:13 +0200

    Refactor

--- src/utils/companyNumberSanitizer.ts
+++ src/utils/companyNumberSanitizer.ts
@@ -5,14 +5,10 @@ export default class CompanyNumberSanitizer {
   private readonly COMPANY_NUMBER_SIZE: number = 8
 
   public sanitizeCompany(companyNumber: string): string {
-    if (!companyNumber) {
-      throw new Error('Company number is required')
-    }
+    const uppercaseCompanyNumber: string = companyNumber.toUpperCase()
+    const strippedCompanyNumber: string = this.stripWhitespaces(uppercaseCompanyNumber)
 
-    companyNumber = companyNumber.toUpperCase()
-    companyNumber = this.stripWhitespaces(companyNumber)
-
-    return this.padNumber(companyNumber)
+    return this.padNumber(strippedCompanyNumber)
   }
 
   private stripWhitespaces(companyNumber: string): string {
@@ -20,18 +16,18 @@ export default class CompanyNumberSanitizer {
   }
 
   private padNumber(companyNumber: string): string {
-    if (/^([a-zA-Z]{2}?)/gm.test(companyNumber)) {
-
-      const leadingChars = companyNumber.substring(0, 2)
-
-      const trailingChars = companyNumber
-        .substring(2, companyNumber.length)
-        .padStart(6, '0')
-
-      return leadingChars + trailingChars
-
-    } else {
+    if (!CompanyNumberSanitizer.isAlphaPrefix(companyNumber)) {
       return companyNumber.padStart(this.COMPANY_NUMBER_SIZE, '0')
     }
+    const leadingChars: string = companyNumber.substring(0, 2)
+    const trailingChars: string = companyNumber
+      .substring(2, companyNumber.length)
+      .padStart(6, '0')
+
+    return leadingChars + trailingChars
+  }
+
+  private static isAlphaPrefix(companyNumber: string): boolean {
+    return /^([a-zA-Z]{2}?)/gm.test(companyNumber)
   }
 }
