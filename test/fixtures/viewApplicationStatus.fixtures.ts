81efa3dcb0e5f4cf31597cad353118cfce3b2710
kheseyongya <kheseyongya@gmail.com>
2021-02-11 17:30:29 +0000

    Fix import ordering issue

--- test/fixtures/viewApplicationStatus.fixtures.ts
+++ test/fixtures/viewApplicationStatus.fixtures.ts
@@ -1,5 +1,6 @@
-import { ViewApplicationStatus, ViewApplicationStatusSignatory } from 'app/models/view/viewApplicationStatus.model'
 import { generateDirectorToRemind, generateDissolutionSession } from './session.fixtures'
+
+import { ViewApplicationStatus, ViewApplicationStatusSignatory } from 'app/models/view/viewApplicationStatus.model'
 
 export function generateViewApplicationStatusModel(): ViewApplicationStatus {
   return {
