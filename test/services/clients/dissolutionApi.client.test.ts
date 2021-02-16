e4a5f8b9bdd10ed710416828436a9949180c96dc
Alfred Bwomezi <ABwomezi@companieshouse.gov.uk>
2021-02-16 10:51:21 +0000

    Remove trailing white-space

--- test/services/clients/dissolutionApi.client.test.ts
+++ test/services/clients/dissolutionApi.client.test.ts
@@ -151,7 +151,7 @@ describe('DissolutionApiClient', () => {
       assert.isTrue(postStub.called)
 
       const [url, coNum, config] = postStub.args[0]
-      
+
       assert.equal(url, reqUrl)
       assert.equal(coNum, COMPANY_NUMBER)
       assert.equal(config.headers.Authorization, API_KEY)
