aa76a26721fcd885beaad737bb0f1ff774c3b1a1
kheseyongya <kheseyongya@gmail.com>
2021-04-06 13:08:55 +0100

    Update file for PR analysis

--- test/controllers/accessibilityStatement.controller.test.ts
+++ test/controllers/accessibilityStatement.controller.test.ts
@@ -7,16 +7,16 @@ import { createApp } from './helpers/application.factory'
 import 'app/controllers/accessibilityStatement.controller'
 import { ACCESSIBILITY_STATEMENT_URI } from 'app/paths'
 
-const pageHeading = 'Accessibility statement for the Apply to strike off and dissolve a company service';
+const pageHeading = 'Accessibility statement for the Apply to strike off and dissolve a company service'
 
 describe('AccessibilityStatementController', () => {
 
     describe('GET request', () => {
         it('should match the heading when trying to access the page', async () => {
-            const app = createApp();
+            const app = createApp()
             await request(app).get(ACCESSIBILITY_STATEMENT_URI).expect(response => {
-                expect(response.text).to.contain(pageHeading);
-            });
-        });
-    });
-});
+                expect(response.text).to.contain(pageHeading)
+            })
+        })
+    })
+})
