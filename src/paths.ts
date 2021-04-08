3c8efa8f123f38cf27374b64d33a21962c784bd4
Lewis Evans <levans@companieshouse.gov.uk>
2021-04-08 10:12:00 +0100

    change GOV.UK text and crown logo to Companies house crest also change hyperlink destination

--- src/paths.ts
+++ src/paths.ts
@@ -5,6 +5,7 @@ export const HEALTHCHECK_URI = `${ROOT_URI}/healthcheck`
 // Redirection
 export const REDIRECT_GATE_URI = `${ROOT_URI}/redirect`
 export const PAYMENT_CALLBACK_URI = `${REDIRECT_GATE_URI}/payment-callback`
+export const CH_HOMEPAGE_URI = `https://find-and-update.company-information.service.gov.uk/`
 
 // Error
 export const ERROR_URI = `${ROOT_URI}/error`
