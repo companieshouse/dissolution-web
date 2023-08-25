2bb6205cbf778fc6161fc41060e6dd0f726f949d
bcullerton <bcullerton@companieshouse.gov.uk>
2023-08-25 09:00:09 +0100

    correct final code smells in sonar

--- src/services/ip-address/ipAddress.service.ts
+++ src/services/ip-address/ipAddress.service.ts
@@ -8,8 +8,6 @@ export default class IpAddressService {
 
     public getIpAddress (req: Request): string {
         const forwardedIpsStr: string = req.headers["x-forwarded-for"] as string
-        const ip: string = forwardedIpsStr.split(",")[0]
-
-        return ip
+        return forwardedIpsStr.split(",")[0]
     }
 }
