cd3bcce53cd9eb39821e38e54c45fc8160e32a24
sbasile-ch <sbasile@companieshouse.gov.uk>
2023-10-18 17:18:37 +0100

    remove commented-out code

--- src/schemas/searchCompany.schema.ts
+++ src/schemas/searchCompany.schema.ts
@@ -2,11 +2,8 @@ import * as Joi from "@hapi/joi"
 import { LocalesService } from "@companieshouse/ch-node-utils"
 
 function formSchema (lang: string) {
-    //const t = LocalesService.getInstance().i18nCh.resolveSingleKey
     const emptyCompanyNumberError = LocalesService.getInstance().i18nCh.resolveSingleKey("err_company_num", lang)
-    //const emptyCompanyNumberError = t("err_company_num", lang)
     const companyIncorrect = LocalesService.getInstance().i18nCh.resolveSingleKey("err_company_incorrect", lang)
-    //const companyIncorrect = t("err_company_incorrect", lang)
 
     return Joi.object({
         companyNumber: Joi.string()
