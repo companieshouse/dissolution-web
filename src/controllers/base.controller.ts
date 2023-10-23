96003fba3e371d6a8317eddaef922f85452f68a3
sbasile-ch <sbasile@companieshouse.gov.uk>
2023-10-23 15:26:51 +0100

    remove log

--- src/controllers/base.controller.ts
+++ src/controllers/base.controller.ts
@@ -5,18 +5,9 @@ import { LocalesService, QUERY_PAR_LANG } from "@companieshouse/ch-node-utils"
 export default abstract class BaseController<T extends object = {}> extends BaseHttpController {
 
     protected async render (template: string, viewModel?: T, status: number = StatusCodes.OK): Promise<string> {
-        console.log("----------X2 (BaseController) ------------")
         const lang = <string> this.httpContext.request.lang
-
-        console.log(`=====1=====BaseController ....(rendering with lang=${lang})`)
-        console.log(this.httpContext.request.session)
-        console.log("=====2=====BaseController")
-
         const data = {}
         Object.assign(data, viewModel)
-        console.log(`====5=== [ ${this.httpContext.request.lang} ] ========`)
-        console.log(data)
-
         Object.assign(data, LocalesService.getInstance().i18nCh.resolveNamespacesKeys(
             lang,
             data
@@ -27,8 +18,6 @@ export default abstract class BaseController<T extends object = {}> extends Base
             checkTilt: "1"
         }
         )
-        console.log("===========>")
-        console.log(data)
         return new Promise<string>((resolve, reject) =>
             this.httpContext.response
                 .status(status)
