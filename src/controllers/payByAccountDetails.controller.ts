8d6a75a89e27beef2f62513d8202e95470aaa0b3
hselvakumar <hselvakumar@companieshouse.gov.uk>
2024-01-24 14:52:17 +0000

    fix

--- src/controllers/payByAccountDetails.controller.ts
+++ src/controllers/payByAccountDetails.controller.ts
@@ -20,72 +20,82 @@ import TYPES from "app/types"
 import FormValidator from "app/utils/formValidator.util"
 
 interface ViewModel {
-  data?: PayByAccountDetailsFormModel
-  errors?: ValidationErrors
+    data?: PayByAccountDetailsFormModel
+    errors?: ValidationErrors
 }
 
 @controller(PAY_BY_ACCOUNT_DETAILS_URI)
 export class PayByAccountDetailsController extends BaseController {
 
-  private readonly ERROR_INCORRECT_CREDENTIALS: string = "Your Presenter ID or Presenter authentication code is incorrect"
+    private readonly ERROR_INCORRECT_CREDENTIALS: string = "Your Presenter ID or Presenter authentication code is incorrect"
 
-  public constructor (
-    @inject(SessionService) private sessionService: SessionService,
-    @inject(DissolutionService) private dissolutionService: DissolutionService,
-    @inject(FormValidator) private validator: FormValidator,
-    @inject(PayByAccountService) private payByAccountService: PayByAccountService,
-    @inject(TYPES.PAY_BY_ACCOUNT_FEATURE_ENABLED) private PAY_BY_ACCOUNT_FEATURE_ENABLED: number
-  ) {
-      super()
-  }
+    public constructor(
+        @inject(SessionService) private sessionService: SessionService,
+        @inject(DissolutionService) private dissolutionService: DissolutionService,
+        @inject(FormValidator) private validator: FormValidator,
+        @inject(PayByAccountService) private payByAccountService: PayByAccountService,
+        @inject(TYPES.PAY_BY_ACCOUNT_FEATURE_ENABLED) private PAY_BY_ACCOUNT_FEATURE_ENABLED: number
+    ) {
+        super()
+    }
 
-  @httpGet('')
-  public async get (): Promise<string | RedirectResult> {
-      if (!this.PAY_BY_ACCOUNT_FEATURE_ENABLED) {
-          return Promise.reject(new NotFoundError("Feature toggle not enabled"))
-      }
+    @httpGet('')
+    public async get(): Promise<string | RedirectResult> {
+        if (!this.PAY_BY_ACCOUNT_FEATURE_ENABLED) {
+            return Promise.reject(new NotFoundError("Feature toggle not enabled"))
+        }
 
-      if (await this.isAlreadyPaid()) {
-          return this.redirect(SEARCH_COMPANY_URI)
-      }
+        if (await this.isAlreadyPaid()) {
+            return this.redirect(SEARCH_COMPANY_URI)
+        }
 
-      return this.renderView()
-  }
+        return this.renderView()
+    }
 
-  @httpPost('')
-  public async post (@requestBody() form: PayByAccountDetailsFormModel): Promise<string | RedirectResult> {
-      const errors: Optional<ValidationErrors> = this.validator.validate(form, payByAccountDetailsSchema)
-      if (errors) {
-          return this.renderView(form, errors)
-      }
+    @httpPost('')
+    public async post(@requestBody() form: PayByAccountDetailsFormModel): Promise<string | RedirectResult> {
+        const errors: Optional<ValidationErrors> = this.validator.validate(form, payByAccountDetailsSchema)
+        if (errors) {
+            return this.renderView(form, errors)
+        }
 
-      const accountNumber: Optional<string> = await this.payByAccountService.getAccountNumber(form)
-      if (!accountNumber) {
-          return this.renderView(form, { presenterAuthCode: this.ERROR_INCORRECT_CREDENTIALS })
-      }
+        const accountNumber: Optional<string> = await this.payByAccountService.getAccountNumber(form)
+        if (!accountNumber) {
+            return this.renderView(form, { presenterAuthCode: this.ERROR_INCORRECT_CREDENTIALS })
+        }
 
-      const dissolutionSession: DissolutionSession = this.sessionService.getDissolutionSession(this.httpContext.request)!
+        const dissolutionSession: DissolutionSession = this.sessionService.getDissolutionSession(this.httpContext.request)!
 
-      await this.dissolutionService.addPayByAccountPaymentData(dissolutionSession, accountNumber)
+        await this.dissolutionService.addPayByAccountPaymentData(dissolutionSession, accountNumber)
 
-      return this.redirect(VIEW_FINAL_CONFIRMATION_URI)
-  }
+        this.updateSession(dissolutionSession)
 
-  private async renderView (data?: PayByAccountDetailsFormModel, errors?: ValidationErrors): Promise<string> {
-      const viewModel: ViewModel = {
-          data,
-          errors
-      }
+        return this.redirect(VIEW_FINAL_CONFIRMATION_URI)
+    }
 
-      return super.render("pay-by-account-details", viewModel, errors ? StatusCodes.BAD_REQUEST : StatusCodes.OK)
-  }
+    private async renderView(data?: PayByAccountDetailsFormModel, errors?: ValidationErrors): Promise<string> {
+        const viewModel: ViewModel = {
+            data,
+            errors
+        }
 
-  private async isAlreadyPaid (): Promise<boolean> {
-      const token: string = this.sessionService.getAccessToken(this.httpContext.request)
-      const dissolutionSession: DissolutionSession = this.sessionService.getDissolutionSession(this.httpContext.request)!
+        return super.render("pay-by-account-details", viewModel, errors ? StatusCodes.BAD_REQUEST : StatusCodes.OK)
+    }
 
-      const dissolution: Optional<DissolutionGetResponse> = await this.dissolutionService.getDissolution(token, dissolutionSession)
+    private updateSession(dissolutionSession: DissolutionSession): void {
+        const updatedSession: DissolutionSession = {
+            ...dissolutionSession
+        }
 
-      return dissolution!.application_status === ApplicationStatus.PAID
-  }
+        this.sessionService.setDissolutionSession(this.httpContext.request, updatedSession)
+    }
+
+    private async isAlreadyPaid(): Promise<boolean> {
+        const token: string = this.sessionService.getAccessToken(this.httpContext.request)
+        const dissolutionSession: DissolutionSession = this.sessionService.getDissolutionSession(this.httpContext.request)!
+
+        const dissolution: Optional<DissolutionGetResponse> = await this.dissolutionService.getDissolution(token, dissolutionSession)
+
+        return dissolution!.application_status === ApplicationStatus.PAID
+    }
 }
