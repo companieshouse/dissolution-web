3e38172adf8497ed306e0941d4899aa7b8bf1419
aoladoyin <aoladoyin@companieshouse.gov.uk>
2025-09-11 15:52:36 +0100

    fix path

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,7 +1,16 @@
 import * as tsConfigPaths from "tsconfig-paths"
 import * as path from "path"
 
+const getPaths = (): { [key: string]: string[] } => {
+    try {
+        const tsConfig = require(path.join(__dirname, "../tsconfig.json"))
+        return tsConfig?.compilerOptions?.paths || { "app/*": ["./*"] }
+    } catch (_e) {
+        return { "app/*": ["./*"] }
+    }
+}
+
 tsConfigPaths.register({
     baseUrl: __dirname,
-    paths: require(path.join(__dirname, "../tsconfig.json")).compilerOptions.paths
+    paths: getPaths()
 })
