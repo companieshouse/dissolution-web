b2e1dc1af299fd51d1a08287907ec87df9c8e295
GruffTrick <gtrick@companieshouse.gov.uk>
2026-08-03 09:17:52 +0100

    apply using paper form controller and page

---
+++ src/controllers/applyUsingPaperForm.controller.ts
@@ -0,0 +1,35 @@
+import { StatusCodes } from "http-status-codes";
+import { controller, httpGet } from "inversify-express-utils";
+import BaseController from "app/controllers/base.controller";
+import { APPLY_USING_PAPER_FORM_DIRECTORS_URI, SEARCH_COMPANY_URI } from "app/paths";
+
+interface ViewModel {
+    backUri?: string;
+    officerType: string;
+}
+
+@controller("")
+export class ApplyUsingPaperFormDirectorsController extends BaseController {
+    public constructor() {
+        super();
+    }
+
+    @httpGet("/close-a-company/apply-using-paper-form-directors")
+    public async getDirectors(): Promise<string> {
+        return this.renderView("director");
+    }
+
+    @httpGet("/close-a-company/apply-using-paper-form-members")
+    public async getMembers(): Promise<string> {
+        return this.renderView("member");
+    }
+
+    private async renderView(officerType: string): Promise<string> {
+        const viewModel: ViewModel = {
+            backUri: SEARCH_COMPANY_URI,
+            officerType,
+        };
+
+        return super.render("apply-using-paper-form", viewModel, StatusCodes.OK);
+    }
+}
