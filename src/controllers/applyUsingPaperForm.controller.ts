cf31b89338c4557d53a0aff12f04dd7ffb305a38
GruffTrick <gtrick@companieshouse.gov.uk>
2026-08-06 14:07:31 +0100

    single uri with dynamic content based on officerType taken from dissolutionSession

--- src/controllers/applyUsingPaperForm.controller.ts
+++ src/controllers/applyUsingPaperForm.controller.ts
@@ -1,29 +1,38 @@
 import { StatusCodes } from "http-status-codes";
-import { controller, httpGet, requestParam } from "inversify-express-utils";
-import BaseController from "app/controllers/base.controller";
-import { SEARCH_COMPANY_URI, APPLY_USING_PAPER_FORM_BASE_URI } from "app/paths";
+import { controller, httpGet } from "inversify-express-utils";
+import { inject } from "inversify";
 
-enum OfficerType {
-    DIRECTOR = "director",
-    MEMBER = "member",
-}
+import BaseController from "app/controllers/base.controller";
+import { APPLY_USING_PAPER_FORM_URI } from "app/paths";
+import OfficerType from "app/models/dto/officerType.enum";
+import Optional from "app/models/optional";
+import DissolutionSession from "app/models/session/dissolutionSession.model";
+import SessionService from "app/services/session/session.service";
 
 interface ViewModel {
-    backUri?: string;
     officerType: string;
 }
 
-@controller(`${APPLY_USING_PAPER_FORM_BASE_URI}-:companyType`)
+@controller(`${APPLY_USING_PAPER_FORM_URI}`)
 export class ApplyUsingPaperFormController extends BaseController {
+    public constructor(@inject(SessionService) private session: SessionService) {
+        super();
+    }
+
     @httpGet("")
-    public async get(@requestParam("companyType") companyType: string): Promise<string> {
-        const officerType = companyType === "members" ? OfficerType.MEMBER : OfficerType.DIRECTOR;
+    public async get(): Promise<string> {
+        let officerType: OfficerType = OfficerType.DIRECTOR;
+        if (this.httpContext.request.session) {
+            const session: Optional<DissolutionSession> = this.session.getDissolutionSession(this.httpContext.request);
+            if (session?.officerType) {
+                officerType = session.officerType;
+            }
+        }
         return this.renderView(officerType);
     }
 
     private async renderView(officerType: OfficerType): Promise<string> {
         const viewModel: ViewModel = {
-            backUri: SEARCH_COMPANY_URI,
             officerType,
         };
 
