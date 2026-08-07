a6c5dd47c27c875fd1227a5641fb62a6d43bdb79
GruffTrick <gtrick@companieshouse.gov.uk>
2026-08-07 11:02:05 +0100

    updated apply using paper form screen

--- src/controllers/applyUsingPaperForm.controller.ts
+++ src/controllers/applyUsingPaperForm.controller.ts
@@ -5,29 +5,28 @@ import { inject } from "inversify";
 import BaseController from "app/controllers/base.controller";
 import { APPLY_USING_PAPER_FORM_URI } from "app/paths";
 import OfficerType from "app/models/dto/officerType.enum";
-import Optional from "app/models/optional";
 import DissolutionSession from "app/models/session/dissolutionSession.model";
 import SessionService from "app/services/session/session.service";
+import CompanyDetails from "app/models/companyDetails.model";
+import ClosableCompanyType from "app/models/mapper/closableCompanyType.enum";
+import CompanyService from "app/services/company/company.service";
 
 interface ViewModel {
     officerType: string;
 }
 
-@controller(`${APPLY_USING_PAPER_FORM_URI}`)
+@controller(APPLY_USING_PAPER_FORM_URI)
 export class ApplyUsingPaperFormController extends BaseController {
-    public constructor(@inject(SessionService) private session: SessionService) {
+    public constructor(
+        @inject(SessionService) private session: SessionService,
+        @inject(CompanyService) private readonly companyService: CompanyService
+    ) {
         super();
     }
 
     @httpGet("")
     public async get(): Promise<string> {
-        let officerType: OfficerType = OfficerType.DIRECTOR;
-        if (this.httpContext.request.session) {
-            const session: Optional<DissolutionSession> = this.session.getDissolutionSession(this.httpContext.request);
-            if (session?.officerType) {
-                officerType = session.officerType;
-            }
-        }
+        let officerType: OfficerType = await this.getOfficerType();
         return this.renderView(officerType);
     }
 
@@ -37,5 +36,21 @@ export class ApplyUsingPaperFormController extends BaseController {
         };
 
         return super.render("apply-using-paper-form", viewModel, StatusCodes.OK);
+    }
+
+    private async getOfficerType(): Promise<OfficerType> {
+        const session: DissolutionSession = this.session.requireDissolutionSession(this.httpContext.request);
+        if (session?.officerType) {
+            return session.officerType;
+        }
+
+        const token: string = this.session.getAccessToken(this.httpContext.request);
+        const companyNumber = this.session.requireDissolutionCompanyNumber(this.httpContext.request);
+        const company: CompanyDetails = await this.getCompanyInfo(token, companyNumber);
+        return company.companyType === ClosableCompanyType.LLP ? OfficerType.MEMBER : OfficerType.DIRECTOR;
+    }
+
+    private async getCompanyInfo(token: string, companyNumber: string): Promise<CompanyDetails> {
+        return this.companyService.getCompanyDetails(token, companyNumber);
     }
 }
