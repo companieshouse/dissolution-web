b897c7fe0ef464cfc21db1e7cfc0aaefc0b7a715
GruffTrick <gtrick@companieshouse.gov.uk>
2026-08-10 13:42:59 +0100

    added requireOfficerType

--- src/controllers/applyUsingPaperForm.controller.ts
+++ src/controllers/applyUsingPaperForm.controller.ts
@@ -5,10 +5,7 @@ import { inject } from "inversify";
 import BaseController from "app/controllers/base.controller";
 import { APPLY_USING_PAPER_FORM_URI } from "app/paths";
 import OfficerType from "app/models/dto/officerType.enum";
-import DissolutionSession from "app/models/session/dissolutionSession.model";
 import SessionService from "app/services/session/session.service";
-import CompanyService from "app/services/company/company.service";
-import Optional from "app/models/optional";
 
 interface ViewModel {
     officerType: string;
@@ -16,16 +13,13 @@ interface ViewModel {
 
 @controller(APPLY_USING_PAPER_FORM_URI)
 export class ApplyUsingPaperFormController extends BaseController {
-    public constructor(
-        @inject(SessionService) private session: SessionService,
-        @inject(CompanyService) private readonly companyService: CompanyService
-    ) {
+    public constructor(@inject(SessionService) private session: SessionService) {
         super();
     }
 
     @httpGet("")
     public async get(): Promise<string> {
-        let officerType: OfficerType = await this.getOfficerType();
+        const officerType = this.session.requireOfficerType(this.httpContext.request);
         return this.renderView(officerType);
     }
 
@@ -35,16 +29,5 @@ export class ApplyUsingPaperFormController extends BaseController {
         };
 
         return super.render("apply-using-paper-form", viewModel, StatusCodes.OK);
-    }
-
-    private async getOfficerType(): Promise<OfficerType> {
-        let officerType: OfficerType = OfficerType.DIRECTOR;
-        if (this.httpContext.request.session) {
-            const session: Optional<DissolutionSession> = this.session.getDissolutionSession(this.httpContext.request);
-            if (session?.officerType) {
-                officerType = session.officerType;
-            }
-        }
-        return officerType;
     }
 }
